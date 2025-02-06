aaf50f814c620c43c05de6ada20d9c5579e8bcd4
ADMIN <ADMIN@DESKTOP-8MO0VGR>
2025-02-06 13:20:13 +0530

    first

---
+++ simplejenkins/src/test/java/demopackeg/JenkinsTest.java
@@ -0,0 +1,13 @@
+package demopackeg;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+
+	@Test
+	public void sample() {
+		Reporter.log("JenkinsTest class executed", true);
+
+	}
+}

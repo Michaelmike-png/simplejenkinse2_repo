73f58d59660ccf92dc93857883da6cf776d355cf
ADMIN <ADMIN@DESKTOP-8MO0VGR>
2025-02-06 13:57:20 +0530

    PollscmTest is added

---
+++ simplejenkins/src/test/java/demopackeg/PollscmTest.java
@@ -0,0 +1,13 @@
+package demopackeg;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollscmTest {
+	
+	@Test
+	public void scmtest() {
+	Reporter.log("PollscmTest added",true);
+	}
+
+}
